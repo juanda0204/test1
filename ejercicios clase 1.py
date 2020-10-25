a5c774e18ab4565bfc6757b1f07171abe9b412f2
juanda0204 <73408764+juanda0204@users.noreply.github.com>
2020-10-24 19:05:42 -0500

    Add files via upload

---
+++ ejercicios clase 1.py
@@ -0,0 +1,3 @@
+Fahr = int(input('inserte su la temperatura en grados Fahrenheit \n'))
+celcius = (Fahr-32)*5/9
+print ('su temperatura en grados Fahrenheit es ',Fahr ,"\n y su temperatura en grados Celcius es ",int(celcius))
